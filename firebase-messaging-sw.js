5b3cbe8a2ded7d074f4e59cd6948b48ad696b5d0
yasmeenalqaduomi-dev <yasmeenalqaduomi@gmail.com>
2025-09-07 08:29:13 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,30 @@
+// استيراد مكتبات Firebase (إصدار compat المناسب للـ service worker)
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
+// تهيئة Firebase
 firebase.initializeApp({
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAi5yQBIXC5_s5xZhkW5Ptw4T2iIraj80I",
+  authDomain: "daily-challenges-1df2d.firebaseapp.com",
+  projectId: "daily-challenges-1df2d",
+  storageBucket: "daily-challenges-1df2d.firebasestorage.app",
+  messagingSenderId: "939866767729",
+  appId: "1:939866767729:web:049e3f30ebfdb60be876d0",
+  measurementId: "G-YL7LTRTVL6"
 });
 
+// Firebase Messaging
 const messaging = firebase.messaging();
 
+// استقبال الإشعارات بالخلفية
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(
-    payload.notification.title,
-    { body: payload.notification.body }
-  );
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png' // ممكن تضيفي أي أيقونة لمشروعك
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
